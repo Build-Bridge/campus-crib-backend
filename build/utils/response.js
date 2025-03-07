9f175188c7ba93d2b9035157622bda5079f96730
DevEmmy <eolaosebikan60@gmail.com>
2025-03-07 01:07:08 +0100

    Enhance successResponse to handle missing payloads with a 400 status response

--- build/utils/response.js
+++ build/utils/response.js
@@ -2,11 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.errorResponse = exports.successResponse = void 0;
 const successResponse = (payload, res) => {
-    return res.json({
-        message: payload.message || "Successful",
-        data: payload.payload,
-        status: 200
-    });
+    if (payload.payload) {
+        return res.json({
+            message: payload.message || "Successful",
+            data: payload.payload,
+            status: 200
+        });
+    }
+    else {
+        return res.status(400).json({
+            message: payload.message,
+            status: 400
+        });
+    }
 };
 exports.successResponse = successResponse;
 const errorResponse = (message, res) => {
--- src/utils/response.ts
+++ src/utils/response.ts
@@ -6,11 +6,20 @@ export interface Payload{
 }
 
 export const successResponse = (payload:  Payload, res: Response) => {
-  return res.json({
+  if(payload.payload){
+    return res.json({
     message:payload.message || "Successful",
     data: payload.payload,
     status:200
   })
+  }
+  
+  else{
+    return res.status(400).json({
+      message: payload.message,
+      status:400
+    })
+  }
 };
 
 export const errorResponse =  (message:  string, res: Response) => {
